e8b1ab5bb9c5391ea738be37c9c5ec6ee19b18d1
Aniket Sharma <aniket965.as@gmail.com>
2025-05-01 16:40:03 +0530

    feat: add funding json

--- scripts/Deployer.s.sol
+++ scripts/Deployer.s.sol
@@ -7,7 +7,7 @@ import {RathFiTachyonForwarder} from "../src/RathFiTachyonForwarder.sol";
 
 contract DeployRathFiForwarder is Script {
     address constant forwarderAddress = 0xE2C5658cC5C448B48141168f3e475dF8f65A1e3e;
-    address constant receiverAddress = 0x2635f1FE263B2976D0c0994b3689a5099D80cc5c;
+    address constant receiverAddress = 0xC4CE7f21f31E115E549B49116D130F0eB15dc726;
     address constant Owner = receiverAddress;
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("KEY");
