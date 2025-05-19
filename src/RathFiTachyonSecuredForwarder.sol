9ba3e62e9c320751644a2eeec7dc9ca712c79373
Aniket Sharma <aniket965.as@gmail.com>
2025-05-19 14:15:03 +0530

    feat: Secured Forwarder

--- src/RathFiTachyonSecuredForwarder.sol
+++ src/RathFiTachyonSecuredForwarder.sol
@@ -2,25 +2,34 @@
  ░█▀▄░█▀█░▀█▀░█░█░░░█▀▀░▀█▀░█▀█░█▀█░█▀█░█▀▀░█▀▀
  ░█▀▄░█▀█░░█░░█▀█░░░█▀▀░░█░░█░█░█▀█░█░█░█░░░█▀▀
  ░▀░▀░▀░▀░░▀░░▀░▀░░░▀░░░▀▀▀░▀░▀░▀░▀░▀░▀░▀▀▀░▀▀▀
-*/ 
+*/
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "solmate/tokens/ERC20.sol"; 
-
+import "solmate/tokens/ERC20.sol";
+import "solady/utils/ECDSA.sol";
 /**
  * @title RathFiTachyonSecuredForwarder
  * @author Aniket965, Rath.Fi (Rath Finance)
  */
-contract RathFiTachyonSecuredForwarder is Ownable {
 
+contract RathFiTachyonSecuredForwarder is Ownable {
     /// @notice signer for secured forwarder address.
     address public signerAddress;
 
     /// @dev Custom error thrown when forwarding transactions fail.
     error TachyonForwarderFailed();
+
+    /// @dev Custom error thrown when the signature is invalid.
+    error InvalidNonce();
+
+    /// @dev Custom error thrown when the signature is invalid.
+    error TachyonInvalidSignature();
+
+    /// @dev nonce bitmap to track used nonces.
+    mapping(uint256 => uint256) public nonceBitmap;
 
     /**
      * @notice Deploys the contract with a specified forwarder and receiver address.
@@ -31,20 +40,67 @@ contract RathFiTachyonSecuredForwarder is Ownable {
         signerAddress = _signerAddress;
     }
 
-    function call(
-        address target,
-        bytes calldata data,
-        uint256 value,
-        bytes calldata signature,
-        uint256 nonce
-    ) external payable onlyOwner {
+
+    /**
+     * @notice Sets the signer address.
+     * @param _signerAddress The new signer address.
+     */
+    function setSignerAddress(address _signerAddress) external onlyOwner {
+        signerAddress = _signerAddress;
+    }
+
+
+    /**
+     * @notice Forwards a call to a target address with the specified data and value.
+     * @param target The target address to forward the call to.
+     * @param data The data to send in the call.
+     * @param value The value to send with the call.
+     * @param nonce The nonce for the transaction.
+     * @param signature The signature authorizing the transaction.
+     */
+    function call(address target, bytes calldata data, uint256 value, uint256 nonce, bytes calldata signature)
+        external
+        payable
+    {
         // Verify the signature
-        bytes32 messageHash = keccak256(abi.encodePacked(block.chainid,nonce,target, data, value));
-        require(recoverSigner(messageHash, signature) == signerAddress, "Invalid signature");
+        bytes32 messageHash = keccak256(abi.encodePacked(block.chainid, nonce, target, data, value));
+
+        _verifySignature(messageHash, signature, signerAddress);
+        _useUnorderedNonce(nonce);
 
         // Forward the call
-        (bool success, ) = target.call{value: value}(data);
-        require(success, "Forwarding failed");
+        (bool success,) = target.call{value: value}(data);
+
+        if (!success) {
+            revert TachyonForwarderFailed();
+        }
+    }
+
+    /**
+     * @notice Forwards multiple calls to a target address with the specified data and value.
+     * @param target The array of target addresses to forward the calls to.
+     * @param data The array of data to send in the calls.
+     * @param value The array of values to send with the calls.
+     * @param nonce The nonce for the transaction.
+     * @param signature The signature authorizing the transaction.
+     */
+    function multiCall(address[] calldata target, bytes[] calldata data, uint256[] calldata value, uint256 nonce, bytes calldata signature)
+        external
+        payable
+    {
+        // Verify the signature
+        bytes32 messageHash = keccak256(abi.encodePacked(block.chainid, nonce, abi.encode(target), abi.encode(data), abi.encode(value)));
+
+        _verifySignature(messageHash, signature, signerAddress);
+        _useUnorderedNonce(nonce);
+
+        // Forward the calls
+        for (uint256 i = 0; i < data.length; i++) {
+            (bool success,) = target[i].call{value: value[i]}(data[i]);
+            if (!success) {
+                revert TachyonForwarderFailed();
+            }
+        }
     }
 
     /**
@@ -54,7 +110,7 @@ contract RathFiTachyonSecuredForwarder is Ownable {
      */
     function TachyonWithdraw(address token, uint256 amount, address receiver) public onlyOwner {
         if (token == address(0)) {
-            (bool success, ) = payable(receiver).call{value: amount}("");
+            (bool success,) = payable(receiver).call{value: amount}("");
             require(success, "ETH transfer failed");
         } else {
             ERC20(token).transfer(receiver, amount);
@@ -62,12 +118,46 @@ contract RathFiTachyonSecuredForwarder is Ownable {
     }
 
     /**
-     * @notice Forwards all calldata and value to `forwarderAddress`.
+     * @notice Returns the bitmap positions for a given nonce.
+     * @dev This function calculates the word position and bit position for a given nonce.
+     * @param nonce The nonce to calculate positions for.
+     * @return wordPos The word position in the bitmap.
+     * @return bitPos The bit position in the word.
      */
-    fallback() external payable {}
+    function bitmapPositions(uint256 nonce) private pure returns (uint256 wordPos, uint256 bitPos) {
+        wordPos = uint248(nonce >> 8);
+        bitPos = uint8(nonce);
+    }
+
+    /**
+     * @notice Marks a nonce as used in the bitmap.
+     * @dev This function flips the bit corresponding to the nonce in the bitmap.
+     * @param nonce The nonce to mark as used.
+     */
+    function _useUnorderedNonce(uint256 nonce) internal {
+        (uint256 wordPos, uint256 bitPos) = bitmapPositions(nonce);
+        uint256 bit = 1 << bitPos;
+        uint256 flipped = nonceBitmap[wordPos] ^= bit;
+
+        if (flipped & bit == 0) revert InvalidNonce();
+    }
+
+    /**
+     * @notice Verifies the signature of a message.
+     * @param messageHash The hash of the message to verify.
+     * @param signature The signature to verify.
+     * @param expectedSigner The expected signer address.
+     */
+    function _verifySignature(bytes32 messageHash, bytes memory signature, address expectedSigner) internal view {
+        if (ECDSA.recover(ECDSA.toEthSignedMessageHash(messageHash), signature) != expectedSigner) {
+            revert TachyonInvalidSignature();
+        }
+    }
+
 
     /**
      * @notice Allows the contract to receive ETH.
      */
+    fallback() external payable {}
     receive() external payable {}
 }
--- src/lib/AuthorizationLib.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-// Library to authenticate the signer address.
-library AuthenticationLib {
-   
-}
